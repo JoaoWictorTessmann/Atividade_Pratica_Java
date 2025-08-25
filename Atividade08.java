3fe366d07193fc9614b581804244c8ce3cc0f771
JOAO WICTOR TESSMANN <joao_w_tessmann@estudante.sesisenai.org.br>
2025-08-25 20:27:03 -0300

    Atividade 8, Verificação de qual numero é maior

---
+++ Atividade08.java
@@ -0,0 +1,15 @@
+public class Atividade08 {
+
+    public static void main(String[] args) {
+        int numero = 11;
+        int numero2 = 11;
+
+        if(numero > numero2) {
+            System.out.println("O número " + numero + " é maior que " + numero2);
+        } else if(numero < numero2) {
+            System.out.println("O número " + numero + " é menor que " + numero2);
+        } else {
+            System.out.println("Os números são iguais.");
+        }
+    }
+}
