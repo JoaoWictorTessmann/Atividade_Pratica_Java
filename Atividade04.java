6c9e8dd7621bbd40396a238d4ce4bdbf8472d490
JOAO WICTOR TESSMANN <joao_w_tessmann@estudante.sesisenai.org.br>
2025-08-25 20:12:20 -0300

    Atividade 4, duas variaveis uma soma e print

---
+++ Atividade04.java
@@ -0,0 +1,11 @@
+public class Atividade04 {
+
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 5;
+
+        int soma = numero1 + numero2;
+
+        System.out.println("A soma de " + numero1 + " e " + numero2 + " é: " + soma);
+    }
+}
