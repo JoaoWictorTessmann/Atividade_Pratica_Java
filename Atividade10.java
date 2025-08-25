a0625815fb8f3352e82609f4cf4fecd4a03d8861
JOAO WICTOR TESSMANN <joao_w_tessmann@estudante.sesisenai.org.br>
2025-08-25 20:33:50 -0300

    Atividade10, Verificação de numero é positivo, negativo ou zero

---
+++ Atividade10.java
@@ -0,0 +1,14 @@
+public class Atividade10 {
+
+    public static void main(String[] args) {
+        int numero = 0;
+
+        if(numero > 0) {
+            System.out.println("O número " + numero + " é positivo.");
+        } else if(numero < 0) {
+            System.out.println("O número " + numero + " é negativo.");
+        } else {
+            System.out.println("O número é zero.");
+        }
+    }
+}
