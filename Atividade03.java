17d20178f76e1facf2bfdaf81b492b3583ebdbfc
JOAO WICTOR TESSMANN <joao_w_tessmann@estudante.sesisenai.org.br>
2025-08-25 20:10:23 -0300

    Foi adicionado a atividade 3, Celcius para F

---
+++ Atividade03.java
@@ -0,0 +1,10 @@
+public class Atividade03 {
+
+    public static void main(String[] args) {
+        int Celsius = 27;
+        double Fahrenheit = (Celsius * 1.8) + 32;
+
+        System.out.println("A temperatura em Fahrenheit é: " + Fahrenheit);
+        System.out.println("A temperatura em Celsius é: " + Celsius);
+    }
+}
