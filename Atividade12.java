6b6e2d44a947d12389033b28db381186db528f28
JOAO WICTOR TESSMANN <joao_w_tessmann@estudante.sesisenai.org.br>
2025-08-25 20:51:01 -0300

    Atividade 12, Loop de 1 a 100 somando os loops e imprimindo o valor final

---
+++ Atividade12.java
@@ -0,0 +1,11 @@
+public class Atividade12 {
+
+    public static void main(String[] args) {
+        int numeroFinal = 0;
+
+        for(int i = 1; i <= 100; i++){
+            numeroFinal += i;
+            System.out.println(numeroFinal);
+    }
+}
+}
