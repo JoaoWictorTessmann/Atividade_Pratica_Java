b6bf72cccfc1398c4c346979657eb4121037f97f
JOAO WICTOR TESSMANN <joao_w_tessmann@estudante.sesisenai.org.br>
2025-08-25 19:56:48 -0300

    Adicionado a primeira atividade da lista, Desafio Par e Impar com output no terminal

---
+++ Atividade01.java
@@ -0,0 +1,12 @@
+public class Atividade01 {
+
+    public static void main(String[] args) {
+        int numero = 3; //Apenas mudar aqui o numero para testar se é impar ou par
+
+        if (numero % 2 == 0) {
+            System.out.println("O número é par!");
+        } else {
+            System.out.println("O número é ímpar!");
+        }
+    }
+}
